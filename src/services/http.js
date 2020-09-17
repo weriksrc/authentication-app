716b128823f6352c5aebb815db9e9c325337c89c
Weriks <weriks.reis.carvalho7@gmail.com>
2020-09-17 18:23:54 -0400

    altera rota

--- src/services/http.js
+++ src/services/http.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const http = axios.create({
 
-  baseURL: process.env.production.VUE_APP_ROOT_API,
+  baseURL: process.env.VUE_APP_ROOT_API,
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
