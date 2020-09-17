82d2ca06c7f623feb66c3a9c39c5715136c423ba
Weriks <weriks.reis.carvalho7@gmail.com>
2020-09-17 16:50:31 -0400

    first commit

---
+++ src/services/http.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+const http = axios.create({
+  baseURL: process.env.VUE_APP_ROOT_API,
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default http;
+
