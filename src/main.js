25eb09e2a06e654dfb1571f325994b79fd1868d3
Weriks <weriks.reis.carvalho7@gmail.com>
2020-09-22 15:06:57 -0400

    add plugins

--- src/main.js
+++ src/main.js
@@ -3,11 +3,29 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
+
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requeresAuth)){
+    if(!store.getters.isloget){
+      next({
+        path: '/',
+        query: {redirect: to.fullPath}
+      })
+    }else{
+      next();
+    }
+  }else{
+    next();
+  }
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
